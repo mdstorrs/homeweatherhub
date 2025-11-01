62d99163d3afdfae38faf02769d7fa3fd035e33e
mdstorrs <141371799+mdstorrs@users.noreply.github.com>
2025-11-02 08:48:59 +1000

    Remove Old projects

--- WSTest/MainModule.vb
+++
@@ -1,7 +0,0 @@
-﻿Module MainModule
-
-    Public Sub ErrorReport(ex As Exception)
-        MessageBox.Show(ex.ToString, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-    End Sub
-
-End Module
